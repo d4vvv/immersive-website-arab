3a9e5acdda019f256976068ad4c286fdee417063
d4vvv <kacper.godula99@gmail.com>
2023-01-27 13:58:33 +0100

    feature: implement requested changes regarding translations

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,25 +1,32 @@
-import Image from "next/image";
-import logo from "../../public/img/logo.png";
+import Image from 'next/image'
+import logo from '../../public/img/logo.png'
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="min-h-[20vh] bg-gradient-primary">
-      <div className="w-full sm:max-w-[640] px-4 md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:mx-w-[1536px] mx-auto">
-        <div className="flex flex-wrap p-[10%] text-white font-Lexend justify-center lg:items-center">
-          <div className="px-4 text-center flex flex-col items-center lg:basis-1/2">
+    <footer className='min-h-[20vh] bg-gradient-primary'>
+      <div className='w-full sm:max-w-[640] px-4 md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1280px] 2xl:mx-w-[1536px] mx-auto'>
+        <div className='flex flex-wrap p-[10%] text-white font-Lexend justify-center lg:items-center'>
+          <div className='px-4 text-center flex flex-col items-center lg:basis-1/2'>
             <Image
               src={logo}
-              alt="logo-immersive-toolbox"
-              className="w-1/5 pb-[3%] align-middle"
+              alt='logo-immersive-toolbox'
+              className='w-1/5 pb-[3%] align-middle'
             />
-            <h4 className="font-medium text-[13px]">Immersive</h4>
+            <h4 className='font-medium text-[13px]'>Immersive</h4>
           </div>
-          <div dir="rtl" className="flex flex-col items-center text-[13px] font-medium text-center gap-1 pb-2 lg:basis-1/2">
-            <h2 className="font-Dela text-3xl mb-2">اتصلوا بنا!</h2>
+          <div
+            dir='rtl'
+            className='flex flex-col items-center text-[13px] font-medium text-center gap-1 pb-2 lg:basis-1/2'
+          >
+            <h2 className='font-Dela text-3xl mb-2'>اتصلوا بنا!</h2>
             <h4>شارع ياسنا (Jasna) 8/20</h4>
-            <h4>مدينة وارسو، الرمز البريدي: 00-013</h4>
+            <h4>
+              مدينة وارسو، الرمز البريدي: <span dir='rtl'>00-013</span>
+            </h4>
             <h4>بولندا</h4>
-            <h4>الهاتف المحمول: +48669743708</h4>
+            <h4>
+              الهاتف المحمول: <span dir='rtl'>48669743708+</span>
+            </h4>
             <h4>البريد الالكتروني: office@immersivegroup.eu</h4>
             <h4>رقم السجل التجاري: 0000853876</h4>
             <h4>رقم تعريف الضرائب: 7272844253</h4>
@@ -28,5 +35,5 @@ export const Footer: React.FC = () => {
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
--- components/main-page/ImmersiveExperience.tsx
+++ components/main-page/ImmersiveExperience.tsx
@@ -1,36 +1,39 @@
-import { ExpBlock } from "./ExpBlock";
-import img1 from "../../public/img/undraw_virtual_reality_y5ig.svg";
-import img2 from "../../public/img/undraw_modern_design_v3wv.svg";
-import img3 from "../../public/img/undraw_teaching_f1cm.svg";
+import { ExpBlock } from './ExpBlock'
+import img1 from '../../public/img/undraw_virtual_reality_y5ig.svg'
+import img2 from '../../public/img/undraw_modern_design_v3wv.svg'
+import img3 from '../../public/img/undraw_teaching_f1cm.svg'
 
 export const ImmersiveExperience: React.FC = () => {
   return (
-    <section id="about">
-      <h2 dir='rtl' className="font-Dela uppercase text-[28px] lg:text-[40px] text-center mt-[3%] py-16 rtl">
-        IMMERSIVE تجربة 
+    <section id='about'>
+      <h2
+        dir='rtl'
+        className='font-Dela uppercase text-[28px] lg:text-[40px] text-center mt-[3%] py-16 rtl'
+      >
+        تجربة IMMERSIVE
       </h2>
-      <div className="grid grid-cols-1 lg:grid-cols-3 lg:max-w-[1100px] mx-auto">
+      <div className='grid grid-cols-1 lg:grid-cols-3 lg:max-w-[1100px] mx-auto'>
         <ExpBlock
           imgSrc={img1}
-          title="IMMERSIVE منصة"
-          text="ننشئ منصتنا لتكنولوجيا الواقع المعزز باستخدام محرك ألعاب &quot;يونيتي&quot; الذي يسمح لنا بالتطوير، فحوصات وتطبيق
+          title='منصة IMMERSIVE'
+          text='ننشئ منصتنا لتكنولوجيا الواقع المعزز باستخدام محرك ألعاب "يونيتي" الذي يسمح لنا بالتطوير، فحوصات وتطبيق
           حلول ممكنة أكثر على مستوى هيئات الواقع المعزز في نفس الوقت. نركّز في الغالب على مايكروسوفت هولولينس
-          2، هواتف ذكية وحواسب لوحية."
+          2، هواتف ذكية وحواسب لوحية.'
         />
         <ExpBlock
           imgSrc={img2}
-          title="تخطيط مركّز على الإنسان"
-          text="واجهة تكنولوجيا الواقع الافتراضي والمعزز هي أساس التقنيات المساعِدة. لذلك نركّز كثيراّ على المستخدمين. نطوّر
+          title='تخطيط مركّز على الإنسان'
+          text='واجهة تكنولوجيا الواقع الافتراضي والمعزز هي أساس التقنيات المساعِدة. لذلك نركّز كثيراّ على المستخدمين. نطوّر
           أجهزتنا عن طريق أفضل ممارسات وبحوث وفقًا للتخطيط المركّز على الإنسان وذلك ما يسمح لنا بتكيّف أجهزتنا
-          بصورة أوسع وأقل إشكالية من قبل ممارسين ومستخدمين صناعيين."
+          بصورة أوسع وأقل إشكالية من قبل ممارسين ومستخدمين صناعيين.'
         />
         <ExpBlock
           imgSrc={img3}
-          title="البحث"
-          text="يعتمد عملنا على نتائج البحوث التي تستخدم لتحليل إشارات فيزيولوجية لاكتشاف التغيرات في ظروف المؤثرة لدى
-          المتدربين."
+          title='البحث'
+          text='يعتمد عملنا على نتائج البحوث التي تستخدم لتحليل إشارات فيزيولوجية لاكتشاف التغيرات في ظروف المؤثرة لدى
+          المتدربين.'
         />
       </div>
     </section>
-  );
-};
+  )
+}
